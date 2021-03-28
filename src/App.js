9df50fb5c41fd451060d8d657c6e07ba53da7d31
wtsaygers1 <wtsaygers1@gmail.com>
2021-03-27 22:43:32 -0400

    background image

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import './App.css';
 
+const divStyle = {
+  backgroundImage: "url(https://cdn.mos.cms.futurecdn.net/wtqqnkYDYi2ifsWZVW2MT4-1200-80.jpg)",
+  backgroundSize: "cover",
+  backgroundPosition: "center",
+  backgroundRepeat: "no-repeat",
+  width: "100vw",
+  height: "100vh"
+};
+
 function App() {
   return (
-    <div className="App">
-  
-    </div>
-  );
+  <div style={divStyle}></div>
+  )
 }
 
 export default App;
